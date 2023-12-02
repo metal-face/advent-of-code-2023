3e54d17e35a4729a237768572f9a5b144c5f8e2f
metal-face <hughesbryan3000@gmail.com>
2023-12-02 16:02:31 -0500

    code and comments cleaned up

--- day-1/part-1/solution.ts
+++ day-1/part-1/solution.ts
@@ -1,16 +1,16 @@
 import { inputArr } from "./input";
 
 function solution(input: string[]): number {
-  // Combine the input string into one giant string separated by '-'
+  // Combine the input array of strings into one giant string separated by '-'
   const giantString: string = input.join("-");
   
-  // An array to store all of the calibration values
-  let calibrationValueArray: number[] = [];
+  // As we accumulate calibration values, they will be stored here
+  let calibrationValues: number[] = [];
   
-  // An array to store the numbers found in the input string
+  // An array to store the numbers found in a single input string
   let tempArr: string[] = [];
 
-  // The total sum of each item in the calibrationValueArray
+  // The total sum of each item in the calibrationValue array
   let totalSum: number = 0;
 
   for (let i = 0; i < giantString.length; i++) {
@@ -20,33 +20,32 @@ function solution(input: string[]): number {
     }
     
     // If at the last item in the giant string and there is a number in the temp array
-    if (giantString[i+1] === undefined && tempArr.length) {
+    if (giantString[i + 1] === undefined && tempArr.length) {
       // Concat that number together with itself, parse to an int and push into calibration value array
-      calibrationValueArray.push(parseInt(tempArr[0].concat(tempArr[0])));
+      calibrationValues.push(parseInt(tempArr[0].concat(tempArr[0])));
       // Empty the array
       tempArr.splice(0, tempArr.length);
     }
 
-    // when the "-" is reached 
+    // If we have reached the separator 
     if (giantString[i] === "-") {
-      
-      // when one item in the temp array
+      // and there is one item in the temp array
       if (tempArr.length === 1) {
         // concat together with itself, parse to an int, push into calibration value array
-        calibrationValueArray.push(parseInt(tempArr[0].concat(tempArr[0])));
+        calibrationValues.push(parseInt(tempArr[0].concat(tempArr[0])));
         // clear the temp array for next input string
         tempArr.splice(0, tempArr.length);  
       }
       
-      // when there are two are more    
+      // or there are two are more items   
       if (tempArr.length >= 2) {
         // concat the first num and last num from temp array, cast to int, push into calibrationValueArray
-        calibrationValueArray.push(parseInt(`${tempArr[0]}${tempArr[tempArr.length - 1]}`));
+        calibrationValues.push(parseInt(`${tempArr[0]}${tempArr[tempArr.length - 1]}`));
         // clear the temp array
         tempArr.splice(0, tempArr.length);
       }     
     
-      // If there's only one element, clear the tempArr
+      // clear the temp array regardless
       tempArr.splice(0, tempArr.length);
     }
   }
@@ -56,8 +55,8 @@ function solution(input: string[]): number {
 
 
   // Iterate over combined numbers and add them all together
-  for (let i = 0; i < calibrationValueArray.length; i++) {
-    totalSum += calibrationValueArray[i];
+  for (let i = 0; i < calibrationValues.length; i++) {
+    totalSum += calibrationValues[i];
   }
 
   return totalSum;
@@ -67,6 +66,4 @@ function exec() {
   console.log(solution(inputArr));
 }
 
-
-
-
+exec();
