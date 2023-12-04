734018919e31498e7add1753c85993114a99439f
metal-face <hughesbryan3000@gmail.com>
2023-12-04 12:37:22 -0500

    Refactor string parsing logic in solution.ts

--- day-1/part-2/solution.ts
+++ day-1/part-2/solution.ts
@@ -8,13 +8,11 @@ function solution(input: string[]): number {
     // As we accumulate calibration values, they will be stored here
     let calibrationValues: number[] = [];
 
+    // A string buffer to build string numbers in an input string
+    let stringBuffer: string = "";
+
     // An array to store the numbers found in a single input string
-    let numArr: string[] = [];
-
-    // An array to store the characters of an input string
-    let charArr: string[] = [];
-
-    let numString: string = "";
+    let numBuffer: string[] = [];
 
     // The total sum of each item in the calibrationValue array
     let totalSum: number = 0;
@@ -22,82 +20,77 @@ function solution(input: string[]): number {
     for (let i = 0; i < giantString.length; i++) {
         // If the ASCII code at 'i' is between 48 and 57, push it into the temp array
         if (giantString.charCodeAt(i) >= 48 && giantString.charCodeAt(i) <= 57) {
-            numArr.push(giantString[i]);
+            numBuffer.push(giantString[i]);
         }
 
         // If the character at 'i' is not a number
         if (giantString.charCodeAt(i) < 48 || giantString.charCodeAt(i) > 57) {
             // and not a "-"
             if (giantString[i] !== "-") {
-                // push it into the char array
-                charArr.push(giantString[i]);
-                // Grow the numString
-                numString = charArr.join("");
+                // add it to the end of the string buffer
+                stringBuffer += giantString[i];
             }
         }
 
-        console.log(charArr, numString);
+        console.log(`i: ${giantString[i]}\n`, `numString: ${stringBuffer}`);
 
         numberMap.forEach((value: number, key: string) => {
-            // and the numString includes a key in the number map
-            if (numString.includes(key)) {
-                // push the value at that key into the calibration values array
-                calibrationValues.push(value);
-
-                // reset the string and array for next possible match
-                numString = "";
-                charArr.splice(0, charArr.length);
+            if (stringBuffer.includes(key)) {
+                numBuffer.push(value.toString());
+                stringBuffer = "";
             }
         });
 
-        // If at the last item in the giant string and there is a number in the temp array
-        if (giantString[i + 1] === undefined && numArr.length) {
-            // Concat that number together with itself, parse to an int and push into calibration value array
-            calibrationValues.push(parseInt(numArr[0].concat(numArr[0])));
-            // Empty the array
-            numArr.splice(0, numArr.length);
-        }
+        console.log(`numArr: ${numBuffer}\n`);
 
         // If we have reached the separator
-        if (giantString[i] === "-") {
-            console.log("reached end of string", calibrationValues);
+        if (giantString[i] === "-" || giantString[i + 1] === undefined) {
             // and there is one item in the temp array
-            if (numArr.length === 1) {
+            if (numBuffer.length === 1) {
+                console.log(`one item in numArr >>> ${parseInt(numBuffer[0].concat(numBuffer[0]))}`);
                 // concat together with itself, parse to an int, push into calibration value array
-                calibrationValues.push(parseInt(numArr[0].concat(numArr[0])));
+                calibrationValues.push(parseInt(numBuffer[0].concat(numBuffer[0])));
                 // clear the temp array for next input string
-                numArr.splice(0, numArr.length);
+                numBuffer.splice(0, numBuffer.length);
             }
 
             // or there are two are more items
-            if (numArr.length >= 2) {
+            if (numBuffer.length >= 2) {
                 // concat the first num and last num from temp array, cast to int, push into calibrationValueArray
-                calibrationValues.push(parseInt(`${numArr[0]}${numArr[numArr.length - 1]}`));
+                console.log(`two or more items in numArr >>> ${numBuffer[0]}${numBuffer[numBuffer.length - 1]}`);
+                calibrationValues.push(parseInt(`${numBuffer[0]}${numBuffer[numBuffer.length - 1]}`));
                 // clear the temp array
-                numArr.splice(0, numArr.length);
+                numBuffer.splice(0, numBuffer.length);
             }
 
             // clear the temp array regardless
-            numArr.splice(0, numArr.length);
-
-            charArr.splice(0, charArr.length);
-            numString = "";
+            numBuffer.splice(0, numBuffer.length);
+            stringBuffer = "";
         }
     }
 
-    // clear the temp arr for next input string
-    numArr.splice(0, numArr.length);
-
     // Iterate over combined numbers and add them all together
     for (let i = 0; i < calibrationValues.length; i++) {
+        console.log("callibration values >>>", calibrationValues[i], "\ntotalSum >>>", totalSum);
         totalSum += calibrationValues[i];
     }
 
     return totalSum;
+    // Should be 54239
 }
 
 function exec() {
+    // const testInput = [
+    //     "two1nine",
+    //     "eightwothree",
+    //     "abcone2threexyz",
+    //     "xtwone3four",
+    //     "4nineeightseven2",
+    //     "zoneight234",
+    //     "7pqrstsixteen",
+    // ];
     console.log(solution(inputArr));
+    // console.log(solution(testInput));
 }
 
 exec();
