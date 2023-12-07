9f55479d481a75286d80f18eb839d2460f1e4db5
metal-face <hughesbryan3000@gmail.com>
2023-12-07 00:22:49 -0500

    Refactor getColors function to calculate the sum of blue, green, and red numbers

--- day-2/part-1/solution.ts
+++ day-2/part-1/solution.ts
@@ -6,16 +6,24 @@ interface Game {
     green: number;
 }
 
-const possibleGames: number[] = [];
+let possibleGamesSum: number = 0;
 
-const blues: RegExp = /[0-9] blue/g;
-const greens: RegExp = /[0-9] green/g;
-const reds: RegExp = /[0-9] red/g;
+const TOTAL_RED_CUBES: number = 12;
+const TOTAL_GREEN_CUBES: number = 13;
+const TOTAL_BLUE_CUBES: number = 14;
 
-function getColors(line: string) {
-    const blueMatches: RegExpMatchArray = line.match(blues);
-    const greenMatches: RegExpMatchArray = line.match(greens);
-    const redMatches: RegExpMatchArray = line.match(reds);
+const blueRegex: RegExp = /[0-9]+ blue/g;
+const greenRegex: RegExp = /[0-9]+ green/g;
+const redRegex: RegExp = /[0-9]+ red/g;
+
+function getColors(line: string, id: number): void {
+    const blueMatches: RegExpMatchArray = line.match(blueRegex);
+    const greenMatches: RegExpMatchArray = line.match(greenRegex);
+    const redMatches: RegExpMatchArray = line.match(redRegex);
+
+    let blueNumbers = blueMatches.map((item) => item.match(/^\d+/)[0]);
+    let greenNumbers = greenMatches.map((item) => item.match(/^\d+/)[0]);
+    let redNumbers = redMatches.map((item) => item.match(/^\d+/)[0]);
 
     const game: Game = {
         red: 0,
@@ -23,11 +31,32 @@ function getColors(line: string) {
         green: 0,
     };
 
-    console.log(blueMatches, greenMatches, redMatches);
+    blueNumbers.forEach((num) => {
+        game.blue += parseInt(num);
+    });
+
+    greenNumbers.forEach((num) => {
+        game.green += parseInt(num);
+    });
+
+    redNumbers.forEach((num) => {
+        game.red += parseInt(num);
+    });
+
+    if (game.red <= TOTAL_RED_CUBES && game.blue <= TOTAL_BLUE_CUBES && game.green <= TOTAL_GREEN_CUBES) {
+        console.log(id, game);
+        possibleGamesSum += id;
+    } else {
+        console.log(game);
+    }
 }
 
-input.forEach((line: string, i: number) => {
-    if (i === 0) {
-        getColors(line);
-    }
-});
+function exec() {
+    input.forEach((line: string, i: number) => {
+        getColors(line, i + 1);
+    });
+
+    return possibleGamesSum;
+}
+
+console.log(exec());
